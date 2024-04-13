2ca496eb608a4c7c263d6ce05cdd1fc4ff16cff8
abhijithu1 <abhiunni9656@gmail.com>
2024-04-13 11:51:16 +0530

    cntlr

--- lib/pharmacy/views/pharmhome.dart
+++ lib/pharmacy/views/pharmhome.dart
@@ -21,28 +21,28 @@ class PharmHome extends StatelessWidget {
       body: Obx(() => Container(
           height: _screenheight * 0.8,
           child: pharmcontroller.bodies[pharmcontroller.selectedpage.value])),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Color.fromARGB(255, 18, 205, 168),
-        currentIndex: pharmcontroller.selectedpage.value,
-        onTap: (index) {
-          pharmcontroller.setindex(index);
-          debugPrint("Index: $index");
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.store),
-            label: "Inventory",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Orders",
-          ),
-        ],
-      ),
+      bottomNavigationBar: Obx(() => BottomNavigationBar(
+            selectedItemColor: Color.fromARGB(255, 18, 205, 168),
+            currentIndex: pharmcontroller.selectedpage.value,
+            onTap: (index) {
+              pharmcontroller.setindex(index);
+              debugPrint("Index: ");
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "Profile",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.store),
+                label: "Inventory",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                label: "Orders",
+              ),
+            ],
+          )),
     );
   }
 }
