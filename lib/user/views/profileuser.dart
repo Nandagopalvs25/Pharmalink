510734a9ef624683fd96ffc1777e44b7945e65aa
abhijithu1 <abhiunni9656@gmail.com>
2024-04-13 15:26:07 +0530

    t

--- lib/user/views/profileuser.dart
+++ lib/user/views/profileuser.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pharmalinkfend/user/controllers/locationcontroller.dart';
 
 class ProfileUser extends StatelessWidget {
   const ProfileUser({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    LocationController ltc = LocationController();
+    return Container(
+      child: Center(
+        child: TextButton(
+          child: Text("Add Location"),
+          onPressed: () {
+            ltc.determinePosition();
+          },
+        ),
+      ),
+    );
   }
 }
