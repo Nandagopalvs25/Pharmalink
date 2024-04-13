264674dcfd5da856185aeb38f972d37b3c4ed90d
Nandagopalvs25 <nandagopalvs12@gmail.com>
2024-04-13 15:27:43 +0530

    new

--- pharmabnd/pharmabnd/settings.py
+++ pharmabnd/pharmabnd/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
+SITE_ID=4
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
